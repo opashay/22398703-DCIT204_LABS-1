e29e22b89fff68ec5fb74a223bc66369f0147705
Patience Owusu <powusu571@st.ug.edu.gh>
2026-06-16 20:46:35 +0100

    Implemented Linear Search and Binary Search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = input.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = input.nextInt();
+
+        // Linear Search
+        int linearResult = Algorithms.linearSearch(array, target);
+
+        System.out.println("\nLinear Search Result:");
+        if (linearResult != -1) {
+            System.out.println("Target found at index " + linearResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        // Sort array before Binary Search
+        Arrays.sort(array);
+
+        // Binary Search
+        int binaryResult = Algorithms.binarySearch(array, target);
+
+        System.out.println("\nBinary Search Result:");
+        if (binaryResult != -1) {
+            System.out.println("Target found at index " + binaryResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        input.close();
     }
 }
